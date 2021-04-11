75e98b9ba86c963063989948671bcde7353e8d24
hastings <hastings.greer@gmail.com>
2021-04-11 18:18:53 -0400

    initial commit

--- src/MotionPlanningProject.jl
+++ src/MotionPlanningProject.jl
@@ -1,5 +1,22 @@
 module MotionPlanningProject
 
-# Write your package code here.
+println("hi")
+struct Position
+    x::Float64
+    y::Float64 
+end
 
+checkpoints = [Position(0, 1), Position(0, 0), Position(2, 0)]
+
+allocation = [2, 3]
+
+frequency = 1000 # Hz
+
+function doit(checkpoints, allocation, frequency)
+    time = 
+end
+
+robot_position=Position(0, 0)
+print(robot_position.x)
+print("\n")
 end
